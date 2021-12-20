d9d15da8539814cff1cbd5b91e6ed4dfa4922597
slava <slava.semik@yandex.ru>
2021-12-20 19:29:42 +0300

    Make start page StartUpScreen

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/src/common/prividers/theme.dart';
 
 import 'generated/l10n.dart';
 import 'src/common/prividers/localization.dart';
-import 'src/view/home/screen/home.dart';
+import 'src/view/start_up/bloc/bloc.dart';
+import 'src/view/start_up/screen/start_up.dart';
 
 class WeatherApp extends StatelessWidget {
   @override
@@ -14,27 +16,28 @@ class WeatherApp extends StatelessWidget {
       child: Builder(
         builder: (context) {
           return StreamBuilder<Locale>(
-              stream: context.read<LocalizationProvider>().localeController.stream,
-              builder: (context, snapshot) {
-                return MaterialApp(
-                  title: 'Score app',
-                  debugShowCheckedModeBanner: false,
-                  locale: snapshot.data,
-                  theme: context.read<ThemeProvider>().theme.appThemeData,
-                  localizationsDelegates: [
-                    S.delegate,
-                    GlobalMaterialLocalizations.delegate,
-                    GlobalCupertinoLocalizations.delegate,
-                    GlobalWidgetsLocalizations.delegate,
-                  ],
-                  supportedLocales: S.delegate.supportedLocales,
-                  // locale: snapshot.data,
-                  home: HomeScreen(
-                    title: '',
-                  ),
-                );
-              });
-        }
+            stream: context.read<LocalizationProvider>().localeController.stream,
+            builder: (context, snapshot) {
+              return MaterialApp(
+                title: 'Score app',
+                debugShowCheckedModeBanner: false,
+                locale: snapshot.data,
+                theme: context.read<ThemeProvider>().theme.appThemeData,
+                localizationsDelegates: [
+                  S.delegate,
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                ],
+                supportedLocales: S.delegate.supportedLocales,
+                home: BlocProvider(
+                  create: (context) => StartUpBloc(),
+                  child: StartUpScreen(),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
