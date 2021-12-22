1ce473e7e5445e14e46935a0498330315cb5c75a
slava <slava.semik@yandex.ru>
2021-12-22 19:47:22 +0300

    Add colors

--- lib/src/common/resource/colors/light.dart
+++ lib/src/common/resource/colors/light.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppLightColors {
-  static const Color primary = Colors.red;
-  static const Color primaryDark = Colors.red;
-  static const Color accent = Colors.red;
-  static const Color accentSecondary = Colors.red;
-  static const Color accentInactive = Colors.red;
-  static const Color background = Colors.red;
-  static const Color surface = Colors.red;
-  static const Color extraPositive = Colors.red;
-  static const Color extraNegative = Colors.red;
+  static const Color primary = Colors.blue;
+  static const Color primaryDark = Colors.blueAccent;
+  static const Color accent = Colors.lightBlue;
+  static const Color accentInactive = Colors.white12;
+  static const Color background = Colors.white30;
+  static const Color surface = Colors.white;
+  static const Color extraPositive = Colors.green;
+  static const Color extraNegative = Colors.redAccent;
   static const Color extraActive = Colors.red;
-  static const Color warning = Colors.red;
-  static const Color onWarning = Colors.red;
-  static const Color onExtra = Colors.red;
-  static const Color onPrimary = Colors.red;
-  static const Color onSurface = Colors.red;
-  static const Color onBackground = Colors.red;
-  static const Color onAccent = Colors.red;
-  static const Color onAccentInactive = Colors.red;
-  static const Color activeShades = Colors.red;
+  static const Color warning = Colors.yellow;
+  static const Color onWarning = Colors.black;
+  static const Color onExtra = Colors.black;
+  static const Color onPrimary = Colors.white;
+  static const Color onSurface = Colors.black;
+  static const Color onBackground = Colors.black;
+  static const Color onAccent = Colors.black;
+  static const Color onAccentInactive = Colors.white;
 }
